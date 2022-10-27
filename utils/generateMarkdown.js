c5847f94fd3d3902e4b00e76d9454cea0cc0d8ba
Davg1700 <davg1700@gmail.com>
2022-10-27 12:50:15 -0700

    LicenseLink

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,20 +3,20 @@
 function renderLicenseBadge(license) {
   if(license === 'Apache-2.0') {
     return `![License: Apache-2.0](https://img.shields.io/badge/License-Apache--2.0-blue)`
-  } else if(license === 'BSD-3' ) {
-    return `![License: MIT](https://img.shields.io/badge/License-BSD--3-blue)`
-  } else if(license === 'GPL' ) {
-    return `![License: GPL](https://img.shields.io/badge/License-GPL-blue)`
   } else if(license === 'LGPL' ) {
     return `![License: LGPL](https://img.shields.io/badge/License-LGPL-blue)`
-  } else if(license === 'MIT' ) {
-    return `![License: MIT](https://img.shields.io/badge/License-MIT-blue)`
-  } else if(license === 'MPL-2.0' ) {
-    return `![License: MPL-2.0](https://img.shields.io/badge/License-MPL--2.0-blue)`
   } else if(license === 'CDDL-1.0' ) {
     return `![License: CDDL-1.0](https://img.shields.io/badge/License-CDDL--1.0-blue)`
   } else if(license === 'EPL-2.0'){
     return `![License: EPL2.0](https://img.shields.io/badge/License-EPL--2.0-blue)`
+  } else if(license === 'BSD-3' ) {
+    return `![License: MIT](https://img.shields.io/badge/License-BSD--3-blue)`
+  } else if(license === 'MPL-2.0' ) {
+    return `![License: MPL-2.0](https://img.shields.io/badge/License-MPL--2.0-blue)`
+  } else if(license === 'GPL' ) {
+    return `![License: GPL](https://img.shields.io/badge/License-GPL-blue)`
+  } else if(license === 'MIT' ) {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-blue)`
   } else {
     return '';
   }
@@ -24,12 +24,39 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'Apache-2.0') {
+    return `(https://opensource.org/licenses/Apache-2.0)`
+  } else if(license === 'MPL-2.0' ) {
+    return `(https://opensource.org/licenses/MPL-2.0)`
+  } else if(license === 'CDDL-1.0' ) {
+    return `(https://opensource.org/licenses/CDDL-1.0)`
+  } else if(license === 'BSD-3' ) {
+    return `(https://opensource.org/licenses/BSD-3-Clause)`
+  } else if(license === 'LGPL' ) {
+    return `(https://opensource.org/licenses/lgpl-license)`
+  } else if(license === 'EPL-2.0'){
+    return `(https://opensource.org/licenses/EPL-2.0)`
+  } else if(license === 'GPL' ) {
+    return `(https://opensource.org/licenses/gpl-license)`
+  } else if(license === 'MIT' ) {
+    return `(https://opensource.org/licenses/MIT)`
+  } else {
+    return false;
+  }
+}
+
+
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
- 
+  if(!license){
+    return '';
+  }
+  return ` ## Liscense`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
